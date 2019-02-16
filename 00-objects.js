24d27980aaf7969659b13e6409dc6a7fe7d036bd
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-02-16 22:43:25 +0400

    Literal Object

--- 00-objects.js
+++ 00-objects.js
@@ -1,39 +1,10 @@
 "use strict";
 console.log(
-    `Creating objects in js is easy but if you want to create and use 100 object
-    It is a very diffcult and extensive task so the best way to do that is creating constructor function
+    `=====>>>Creating objects in js is (easy) but if you want to create and use 100 object
+    It is a very not easy and extensive task so the best way to do that is creating constructor function<<<====
     `
 )
 //defining literal object
-let user = {
-    firstName: "Ahmed",
-    lastName: "Mahmmoud",
-    email: "ahmed.m.web.dev@gmail.com",
-    login() {
-        console.log(`${this.firstName} ${this.lastName} has been logged in`);
-    },
-    logout() {
-        console.log(`${this.firstName} ${this.lastName} has been logged out`);
-    }
-};
-setTimeout(() => {
-    user.login();
-}, 1000);
-setTimeout(() => {
-    user.logout();
-}, 4000);
-//adding properties and methods to object
-user.age = 27;
-user.salary = 5000;
-user.tax = 0.01;
-user.netSalary = function () {
-    let total = this.salary - (this.tax * this.salary);
-    return total;
-}
-console.log(user.age);
-console.log(user.netSalary());
-
-//creating literal objects
 const book1 = {
     title: "Learn OOP in js",
     author: "Ahmed Mahmoud",
@@ -43,9 +14,8 @@ const book1 = {
         return summary;
     }
 }
-
 const book2 = {
-    title: "Learn logics in js",
+    title: "Learn basics in js",
     author: "Ahmed Mahmoud",
     content: "Learn basic programming in javascript",
     summary: function () {
@@ -53,13 +23,29 @@ const book2 = {
         return summary;
     }
 }
+const book3 = {
+    title: "Learn functions in js",
+    author: "Ahmed Mahmoud",
+    content: "Learn functions programming in javascript",
+    summary: function () {
+        let summary = `Title:${this.title} WrittenBy:${this.author} Book-contents:${this.content}`;
+        return summary;
+    }
+}
+console.log("=============================");
 console.log(book1.summary());
 console.log("=============================");
 console.log(book2.summary());
+console.log("=============================");
+console.log(book3.summary());
+
+
 //get values and keys of an object
 console.log("=============================");
+console.log("==========Object Values==========");
 console.log(Object.values(book1));
 console.log("=============================");
+console.log("==========Object Keys==========");
 console.log(Object.keys(book1));
 console.log("=============================");
 //check if object has a key and get its value
